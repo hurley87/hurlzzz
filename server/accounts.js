9ef8e1b697b28d6febda7e45083309a386f5bbca
david hurley <dhurls99@gmail.com>
2015-07-05 18:50:02 -0400

    ok now here we go

--- server/accounts.js
+++ server/accounts.js
@@ -11,32 +11,32 @@ ServiceConfiguration.configurations.insert({
 var ig = Meteor.npmRequire('instagram-node').instagram(); 
 ig.use({ client_id: Meteor.settings.instagram_id,
      client_secret: Meteor.settings.instagram_secret });
-// var Future = Npm.require('fibers/future');
+var Future = Npm.require('fibers/future');
 
 Accounts.onCreateUser(function(options, user) {	
-  // var recentPosts = new Future();
-  // var stats = new Future();
-  // var data = new Future();
+  var recentPosts = new Future();
+  var stats = new Future();
+  var data = new Future();
   
 
   if (options.profile)
   user.profile = options.services.instagram;
 
   ig.user_media_recent( user.profile.id, function(err, medias, pagination, remaining, limit) {
-  	// recentPosts.return(medias);
+  	recentPosts.return(medias);
   }); 
 
   ig.user(user.profile.id, function(err, result, remaining, limit) {
-  	// stats.return(result.counts);
+  	stats.return(result.counts);
   });
 
   Meteor.call('getData', recentPosts.wait(), stats.wait(), function(error, result) {
-    // data.return(result);
+    data.return(result);
   });
 
-  // user.profile.stats = stats.wait();
-  // user.profile.posts = recentPosts.wait();
-  // user.profile.data = data.wait();
+  user.profile.stats = stats.wait();
+  user.profile.posts = recentPosts.wait();
+  user.profile.data = data.wait();
   
   // var hashtags = user.profile.data.hashtagList;
   // var tags = [];
