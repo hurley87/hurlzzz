8e8195b3698865bc808bac77440f50578d8255ec
david hurley <dhurls99@gmail.com>
2015-07-12 14:43:07 -0400

    fuckkkkk

--- server/publish.js
+++ server/publish.js
@@ -1,4 +1,4 @@
 Meteor.publish('allLeaderboardUsers', function() {
-  var data = Meteor.users.find({}, { sort: { 'profile.data.postValue': 1 }});
+  var data = Meteor.users.find({}, { sort: { 'profile.data.postValue': -1 }});
   return data;
 });
