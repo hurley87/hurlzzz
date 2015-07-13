f73b23cbb4c8a796bf17220d499e4940b3310aed
david hurley <dhurls99@gmail.com>
2015-07-13 09:14:55 -0400

    ok cool

--- client/templates/views/profile/profile.js
+++ client/templates/views/profile/profile.js
@@ -88,16 +88,16 @@ Template.dashboard.onRendered(function() {
 });  
 
   Meteor.Spinner.options = {
-    lines: 5, 
-    length: 15, // The length of each line
-    width: 10, // The line thickness
+    lines: 10, 
+    length: 40, // The length of each line
+    width: 15, // The line thickness
     radius: 15, // The radius of the inner circle
     corners: 1, // Corner roundness (0..1)
     rotate: 0, // The rotation offset
     direction: 1, // 1: clockwise, -1: counterclockwise
     color: '#3498DB', // #rgb or #rrggbb
-    speed: 1, // Rounds per second
-    trail: 60, // Afterglow percentage
+    speed: 1.4, // Rounds per second
+    trail: 75, // Afterglow percentage
     shadow: false, // Whether to render a shadow
     hwaccel: false, // Whether to use hardware acceleration
     className: 'spinner', // The CSS class to assign to the spinner
