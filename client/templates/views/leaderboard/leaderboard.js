10b9f2a7e3a4b1b0bb8c9ffdb0b3ee6cb797fd30
david hurley <dhurls99@gmail.com>
2015-07-21 08:59:27 -0400

    another way - inifite scroll

--- client/templates/views/leaderboard/leaderboard.js
+++ client/templates/views/leaderboard/leaderboard.js
@@ -1,33 +1,41 @@
+
+
 Template.leaderboard.helpers({
-	users: function() {
-		return Meteor.users.find({}, {
-      sort: {
-        'profile.data.postValue': -1
-      }
-    });
-	},
-  elites: function() {
-    return Meteor.users.find({
-      'profile.data.postValue' : {
-        $gt : 100
-      }
+    setup : function() {
+        var _columns = [
+            {
+                name : 'Rank',
+                varName : 'rowNum',
+                class : 'row-number custom-td',
+                transform : rowNumTransform
+            },
+            {
+                name : 'Followers',
+                varName : 'data_1',
+                class : 'data-1 custom-td'
+            },
+            {
+                name : 'Gender',
+                varName : 'data_2',
+                class : 'data-2 custom-td'
+            }
+        ];
+        var _css = {
+            table_class : 'custom-table',
+            row_class : 'custom-row'
+        };
+        return _.extend(this, { columns : _columns , css : _css });
     },
-    {
-      sort: { 'profile.data.postValue': -1 }
-    });
-  },
-  allstars: function() {
-     return Meteor.users.find({
-      'profile.data.postValue' : {
-        $lt : 100
-      }
-    },
-    {
-      sort: { 'profile.data.postValue': -1 }
-    });   
-  },
-  eliteCount: function() {
-    return Meteor.users.find().count();
-  } 
+    count : function() {
+        var count = Meteor.users.find().count();
+        console.log(count);
+        if (count == undefined) return 0;
+        return count.count;
+    }
 });
 
+var rowNumTransform = function(_data) {
+    var data = _data;
+    var html = new Handlebars.SafeString(data);
+    return html;
+}
--- table.js
+++ table.js
@@ -1,22 +1,39 @@
-TabularTables = {};
+// TabularTables = {};
 
-Meteor.isClient && Template.registerHelper('TabularTables', TabularTables);
+// Meteor.isClient && Template.registerHelper('TabularTables', TabularTables);
 
-TabularTables.Users = new Tabular.Table({
-  name: "Users",
-  collection: Meteor.users,
-  columns: [
-    {data: "profile.username", title: "Handle"},
-    {data: "profile.data.postValue", title: "Value"},
-    {data: "profile.other.city", title: "City"},
-    {data: "profile.other.country", title: "Country"},
-    {data: "profile.other.age", title: "Birthdate"},
-    {data: "profile.other.gender", title: "Gender"},
-    {data: "profile.stats.followed_by", title: "Followers"},
-    {data: "profile.data.avgLikes", title: "Avg Likes"},
-    {data: "profile.data.avgComments", title: "Avg Comments"},
-    {data: "profile.data.engagement", title: "Engagement"},
-    {data: "profile.data.consistency", title: "Consistency"},
-  ],
-  responsive: true
-});
+// TabularTables.Users = new Tabular.Table({
+//   name: "Users",
+//   collection: Meteor.users,
+//   columns: [
+//     {data: "profile.username", title: "Handle"},
+//     {data: "profile.data.postValue", title: "Value"},
+//     {data: "profile.other.city", title: "City"},
+//     {data: "profile.other.country", title: "Country"},
+//     {data: "profile.other.age", title: "Birthdate"},
+//     {data: "profile.other.gender", title: "Gender"},
+//     {data: "profile.stats.followed_by", title: "Followers"},
+//     {data: "profile.data.avgLikes", title: "Avg Likes"},
+//     {data: "profile.data.avgComments", title: "Avg Comments"},
+//     {data: "profile.data.engagement", title: "Engagement"},
+//     {data: "profile.data.consistency", title: "Consistency"},
+//   ],
+//   responsive: true
+// });
+
+exampleCollection = new Meteor.Collection('_example_collection');
+
+if (Meteor.isServer) {
+    Meteor.startup(function() {
+        exampleCollection.remove({});
+        Meteor.defer(function() {
+        	var users = Meteor.users.find({}, { 'profile.data.postValue': -1 }).fetch();
+            console.log(users);
+            var count = 1;
+	    	users.forEach(function(user) {
+	    		exampleCollection.insert({ rowNum : count, data_1 : user.profile.stats.followed_by, data_2 : user.profile.other.gender });
+	    		count += 1;
+	    	});   
+        });
+    });
+}
