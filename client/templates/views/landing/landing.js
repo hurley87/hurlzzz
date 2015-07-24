7327d2eb9e2cfc22f2d8468ceb2bf002fd157777
david hurley <dhurls99@gmail.com>
2015-07-24 10:46:56 -0400

    new changes

--- client/templates/views/landing/landing.js
+++ client/templates/views/landing/landing.js
@@ -12,3 +12,73 @@
       });
     }
   });
+
+Template.updateUser.onRendered(function() {
+    $('#datapicker2').datepicker();
+});
+
+Template.updateUser.helpers({
+    user: function() {
+      if(Meteor.userId()) {
+        return Meteor.users.findOne(Meteor.userId()).profile;
+      }  
+    }
+});
+
+Template.updateUser.events({
+    'submit #update': function(evt, temp) {
+        evt.preventDefault();
+        var userDetails = {
+                city: $('#city').val(),
+                country: $('#country').val(),
+                email: $('#email').val(),
+                sex: $("#gender").val(),
+                dob: $('#datapicker2').val()
+            };
+        var user = Meteor.users.findOne(Meteor.userId());
+        Meteor.call('updateUser', userDetails, user);
+        Router.go('/');
+    },
+    'input #country': function(evt, templ) {
+      evt.preventDefault();
+      Session.set('country', $('#country').val());
+    },
+    'input #city': function(evt, templ) {
+      evt.preventDefault();
+      Session.set('city', $('#city').val());
+    },
+    'input #gender': function(evt, templ) {
+      evt.preventDefault();
+      Session.set('gender', $('#gender').val());
+    },
+    'change #datapicker2': function(evt, templ) {
+      evt.preventDefault();
+      Session.set('datapicker2', $('#datapicker2').val());
+    }
+});
+
+Template.instaWorth.helpers({
+  country: function() {
+    return Session.get('country');
+  },
+  city: function() {
+    return Session.get('city');
+  },
+  gender: function() {
+    return Session.get('gender');
+  },
+  age: function() {
+    return Session.get('datapicker2');
+  },
+  user: function() {
+    var id = Router.current().params._id;
+    if(!id) { id = Meteor.users.findOne(Meteor.userId()).profile.username}
+    return Meteor.users.findOne({ 'profile.username' : id }).profile;
+  },
+  thisUser: function() {
+    var thisId = Router.current().params._id;
+    if(!thisId) { thisId = Meteor.users.findOne(Meteor.userId()).profile.username}
+    var myId = Meteor.users.findOne({ 'profile.username' : id }).profile.username;
+    return thisId == myId;
+  }  
+});
