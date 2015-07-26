7c7e71d8d032b8729bfce0f4504cabd4bc815369
david hurley <dhurls99@gmail.com>
2015-07-26 13:55:58 -0400

    login time

--- client/templates/views/leaderboard/leaderboard.js
+++ client/templates/views/leaderboard/leaderboard.js
@@ -78,6 +78,23 @@ Template.leaderboard.helpers({
   }
 });
 
+Template.leaderboard.events({
+  'click #signMeUp': function(evt, temp) {
+      evt.preventDefault();
+      Meteor.loginWithInstagram(function (err, res) {
+        console.log(res);
+        if (err !== undefined) {
+          console.log('sucess ' + res);
+        } else {
+          console.log('login failed ' + err);
+        }
+        var users = Meteor.users.find({}, { sort: { 'profile.data.postValue': -1 } }).fetch();
+        Meteor.call('setRank', users);
+        Router.go('/edit'); 
+      });
+  }
+})
+
 Template.leaderboard.onRendered(function() {
   $('table').css({'width': '100%'});
   $('.dataTables_length').parent().hide();
