7cf3b3ba188799009b26905d5b55a8dc51aeff60
david hurley <dhurls99@gmail.com>
2015-09-01 10:17:02 -0400

    cool

--- client/config/router.js
+++ client/config/router.js
@@ -11,6 +11,14 @@ Router.route('/', {
   }
 });
 
+Router.route('/everyone', {
+  template: 'landingPage',
+  loadingTemplate: 'loading',
+  subscriptions: function() {
+    this.subscribe('allLeaderboardUsers');
+  }
+});
+
 Router.route('/edit', {
   template: 'edit',
   loadingTemplate: 'loading'
